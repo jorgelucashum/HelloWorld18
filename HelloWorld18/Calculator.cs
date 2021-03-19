e0b3e203e172e75eb2f5fa9b24a8edbf851313e0
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-19 15:20:49 -0300

    Add project files.

---
+++ HelloWorld18/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld18
+{
+    class Calculator
+    {
+        // AVISO!! 'ref' e 'out' são consideradas 'code smells' (design ruim) e devem ser evitadas.
+        // elas só existe porque é necessário para integrar com a linguagem 'C++'
+
+        // Modificadores de parâmetros
+        public static void Triple(ref int x) // 'ref' serve para fazer referencia à variável original da função 'main' mesmo sem retorno, a variável passada como parâmetro deve ter sido iniciada.
+        {
+            x = x * 3;
+        }
+
+        public static void Triple2(int origin, out int result) // 'out' serve para fazer referencia à variável original só que sem necessidade da variável passada como parâmetro ter sido iniciada.
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ HelloWorld18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld18
+{
+    class Program
+    {
+        // AVISO!! 'ref' e 'out' são consideradas 'code smells' (design ruim) e devem ser evitadas.
+        // elas só existe porque é necessário para integrar com a linguagem 'C++'
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Calculator.Triple(ref a); // Modificador de parâmetro 'ref' sendo usado.
+            Console.WriteLine(a);
+
+            a = 5;
+            int triple;
+            Calculator.Triple2(a, out triple); // Modificador de parâmetro 'out' sendo usado.
+            Console.WriteLine(triple);
+        }
+    }
+}
